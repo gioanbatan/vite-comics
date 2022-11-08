9932e4eecf0261cdb283329f7f209c8586068e9e
gioanbatan <giovannibassani@hotmail.com>
2022-11-08 19:06:45 +0100

    Add: Banner dynamic items

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -6,9 +6,35 @@ export default {
             bannerElements: [
                 {
                     name: "DIGITAL COMICS",
-                    image: "url.image",
-                }
+                    image: "buy-comics-digital-comics.png",
+                    href: "#"
+                },
+                {
+                    name: "DC MERCHANDISE",
+                    image: "buy-comics-merchandise.png",
+                    href: "#"
+                },
+                {
+                    name: "SUBSCRIPTIONS",
+                    image: "buy-comics-subscriptions.png",
+                    href: "#"
+                },
+                {
+                    name: "COMIC SHOP LOCATOR",
+                    image: "buy-comics-shop-locator.png",
+                    href: "#"
+                },
+                {
+                    name: "DC POWER VISA",
+                    image: "buy-dc-power-visa.svg",
+                    href: "#"
+                },
             ]
+        }
+    },
+    methods: {
+        getImagePath(imgPath) {
+            return new URL(imgPath, import.meta.url).href;
         }
     }
 }
@@ -18,36 +44,11 @@ export default {
     <div class="banner-section">
         <div class="container">
             <div class="banner-container flex-v-center">
-                <ul class="flex-between">
-                    <li>
-                        <a class="flex-v-center" href="">
-                            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                            <span>TEST</span>
-                        </a>
-                    </li>
-
-                    <li>
-                        <a class="flex-v-center" href="">
-                            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                            <span>TEST</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="flex-v-center" href="">
-                            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                            <span>TEST</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="flex-v-center" href="">
-                            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                            <span>TEST</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a class="flex-v-center" href="">
-                            <img src="../assets/img/buy-comics-digital-comics.png" alt="">
-                            <span>TEST</span>
+                <ul class="flex-between flex-v-center">
+                    <li v-for="link in bannerElements">
+                        <a class="flex-v-center" :href="link.href">
+                            <img :src="getImagePath(`../assets/img/${link.image}`)" :alt="link.name">
+                            <span>{{ link.name }}</span>
                         </a>
                     </li>
                 </ul>
@@ -66,6 +67,7 @@ export default {
     width: 100%;
     height: 200px;
 
+    font-size: 0.9rem;
     color: var(--col-white);
 }
 
@@ -74,10 +76,12 @@ ul {
 }
 
 a img {
-    width: 40px;
+    max-width: 50px;
+    max-height: 50px;
 }
 
 a span {
+    display: inline-block;
     padding-left: 1rem;
 }
 </style>
