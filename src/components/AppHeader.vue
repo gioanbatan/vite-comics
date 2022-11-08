56e95e5815525902f0c83ac082df251162667117
gioanbatan <giovannibassani@hotmail.com>
2022-11-08 16:33:23 +0100

    Add: components AppHeader, AppMain and test

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<template>
+    <h2>
+        TESTHEADER
+    </h2>
+</template>
+
+<style scoped>
+h2 {
+    background-color: brown;
+    height: 100px;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    name: "AppMain",
+}
+</script>
+
+<template>
+    <h2>Test Main</h2>
+</template>
+
+<style>
+h2 {
+    background-color: aqua;
+    height: 200px;
+}
+</style>
