cfd6fd7a0dd8f7ed1d819f5ee734377d43f5f498
gioanbatan <giovannibassani@hotmail.com>
2022-11-09 17:25:38 +0100

    Add: Props don't working

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@ import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 import AppBanner from "./components/AppBanner.vue";
 import AppFooter from "./components/AppFooter.vue";
+import ComicsCard from "./components/ComicsCard.vue";
 
 export default {
   components: {
     AppHeader,
     AppMain,
     AppBanner,
-    AppFooter
+    AppFooter,
+    ComicsCard
   }
 }
 </script>
